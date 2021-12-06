c77b717ffc0be8dbce1ddf8eb9248a4bdac74723
kryger <krygergo@gmail.com>
2021-12-06 23:32:00 +0100

    unused dependency

--- frontend/src/user/UserRoute.tsx
+++ frontend/src/user/UserRoute.tsx
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Switch, Route } from 'react-router-dom'
+import { Route } from 'react-router-dom'
 import Dashboard from '../dashboard/Dashboard'
 import Administrator from '../dashboard/Administrator';
 import AdminRoute from '../dashboard/AdminRoute';
