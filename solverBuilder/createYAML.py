08c87e4beb6715135e3cde64b7da3cfa30fe975f
kberg18 <kberg18@student.sdu.dk>
2021-12-09 11:18:51 +0100

    forgot to close the files in createYAML.py. Now updated

--- solverBuilder/createYAML.py
+++ solverBuilder/createYAML.py
@@ -4,23 +4,22 @@ if len(sys.argv) != 2 and len(sys.argv) != 3:
     raise Exception("Positional arguments must be: image name, tag (optional)")
 
 # read the template file
-file = open("podTemplate.yaml", "r")
-content = file.read()
+with open("podTemplate.yaml", "r") as template:
+    content = template.read()
+    # insert the image name
+    imageName = sys.argv[1]
+    content = content.replace("IMAGE", imageName)
 
-# insert the image name
-imageName = sys.argv[1]
-content = content.replace("IMAGE", imageName)
+    # insert the tag if one is provided 
+    tag = "latest"
+    try:
+        tag = sys.argv[2]
+    except:
+        pass
+        
+    content = content.replace("TAG", tag)
 
-# insert the tag if one is provided 
-tag = "latest"
-try:
-    tag = sys.argv[2]
-except:
-    pass
-    
-content = content.replace("TAG", tag)
-
-# write to new YAML file
-newYAML = open("pod.yaml", "w")
-newYAML.seek(0)
-newYAML.write(content)
+    # write to new YAML file
+    with open("pod.yaml", "w") as newYAML:
+        newYAML.seek(0)
+        newYAML.write(content)
