e00b9c5431312dd6201abb24851e89bd5af8a6e1
Simon Andersen <siman16@student.sdu.dk>
2021-12-09 21:32:35 +0100

    updated signup and login

--- backend/test/auth/signup.test.ts
+++ backend/test/auth/signup.test.ts
@@ -1,5 +1,8 @@
+import { Cookie } from "express-session";
 import supertest from "supertest"
 import app from "../../src/app";
+import { getUserByUsername } from "../../src/user/userService";
+
 
 jest.mock("../../src/user/userService", () => ({
     addUser: jest.fn((username: string, _: string) => username !== "test" ? "userId" : undefined)
@@ -7,13 +10,16 @@ jest.mock("../../src/user/userService", () => ({
 
 describe("POST /signup", () => {
     it("Returns status code 201 if non existing username is passed", async () => {
+        
         const res = await supertest(app)
             .post("/signup")
             .send({
                 username: "username",
                 password: "password"
-            });
-        expect(res.statusCode).toBe(201);
+            });  
+        expect(res.statusCode).toBe(201)
+        
+        
     });
 
     it("Returns status code 403 if existing username is passed", async () => {
@@ -26,3 +32,34 @@ describe("POST /signup", () => {
         expect(res.statusCode).toBe(403)
     });
 });
+
+describe("Check users without cookie", 
+    () =>  test("Check users", ()  => 
+        supertest(app).get("/user").expect(401)
+    )
+);
+
+
+/*
+describe("Get /user", () => {
+    it("Succes", async () => {
+        const res = await supertest(app)
+            .post("/signup")
+            .send({
+                username: "username",
+                password: "password"
+            });
+
+        const cookie = res.header["set-cookie"][1] as string;
+        const cookie1 = (cookie.slice("cloudsolver.sid=".length).split(";")[0]);
+
+
+        const res1 = await supertest(app).get("/user").set("Authorization", cookie1);
+
+        expect(res1.statusCode).toBe(200);
+
+
+    });
+
+});
+*/
