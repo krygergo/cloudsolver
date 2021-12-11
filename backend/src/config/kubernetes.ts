78d80cdaeb2d2feb51036d617c09ce410cdea511
majegit <casper@majgaard.dk>
2021-12-11 18:05:26 +0100

    job-creator role + self killing kaniko pods

--- backend/src/config/kubernetes.ts
+++ backend/src/config/kubernetes.ts
@@ -1,8 +1,9 @@
-import { KubeConfig, CoreV1Api } from "@kubernetes/client-node";
+import { KubeConfig, CoreV1Api, BatchV1Api } from "@kubernetes/client-node";
 
 const config = new KubeConfig();
 config.loadFromDefault();
 
 const api = config.makeApiClient(CoreV1Api);
+const api2 = config.makeApiClient(BatchV1Api)
 
-export default () => ({ config, api });
+export default () => ({ config, api, api2 });
