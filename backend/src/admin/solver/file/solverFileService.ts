87c7c218b3a9d55b3b68b1255df65bcb6ce6d2fc
majegit <casper@majgaard.dk>
2021-12-13 21:22:12 +0100

    JobCreator for kaniko

--- backend/src/admin/solver/file/solverFileService.ts
+++ backend/src/admin/solver/file/solverFileService.ts
@@ -2,43 +2,49 @@ import { UploadedFile } from "express-fileupload";
 import storage from "../../../config/googleStorage"
 import { PassThrough } from "stream"
 import k8s from "../../../config/kubernetes"
-import { V1Pod } from "@kubernetes/client-node";
+import { V1Job } from "@kubernetes/client-node";
 
 const bucket = storage().bucket("cloudsolver-334113.appspot.com");
 
-const kanikoPod = (solvername: string): V1Pod => ({
-    apiVersion: "v1",
-    kind: "Pod",
+const kanikoJob = (solvername: string): V1Job => ({
+    apiVersion: "batch/v1",
+    kind: "Job",
     metadata: {
-        name: `kaniko-${solvername}`
+        name: `kaniko-job-${solvername}`
     },
     spec: {
-        containers: [{ 
-            name: "kaniko",
-            image: "gcr.io/kaniko-project/executor:edge",
-            args: [
-                "--dockerfile=./Dockerfile",
-                `--context=gs://cloudsolver-334113.appspot.com/solvers/${solvername}.tar.gz`,
-                `--destination=gcr.io/cloudsolver-334113/${solvername}:latest`
-            ],
-            volumeMounts: [{
-                name: "keys",
-                mountPath: "/keys",
-                readOnly: true
-            }],
-            env: [{
-                name: "GOOGLE_APPLICATION_CREDENTIALS",
-                value: "/keys/google-api-key.json"
-            }],
-        }],
-        restartPolicy: "Never",
-        volumes: [{
-            name: "keys",
-            secret: {
-                secretName: "google-api-key"
+        template: {
+            spec: {
+                containers: [{ 
+                    name: `kaniko-container-${solvername}`,
+                    image: "gcr.io/kaniko-project/executor:edge",
+                    args: [
+                        "--dockerfile=./Dockerfile",
+                        `--context=gs://cloudsolver-334113.appspot.com/solvers/${solvername}.tar.gz`,
+                        `--destination=eu.gcr.io/cloudsolver-334113/${solvername}:latest`
+                    ],
+                    volumeMounts: [{
+                        name: "keys",
+                        mountPath: "/keys",
+                        readOnly: true
+                    }],
+                    env: [{
+                        name: "GOOGLE_APPLICATION_CREDENTIALS",
+                        value: "/keys/google-api-key.json"
+                    }],
+                }],
+                restartPolicy: "Never",
+                volumes: [{
+                    name: "keys",
+                    secret: {
+                        secretName: "google-api-key"
+                    }
+                }],
             }
-        }]
-    },
+        },
+        activeDeadlineSeconds: 60 * 30, // Must finish within 30min
+        ttlSecondsAfterFinished: 0
+    }
 })
 
 
@@ -57,6 +63,6 @@ export const addSolverFile = async (fileUpload: UploadedFile) => {
         passthroughStream.pipe(file.createWriteStream()).on('finish', () => {})}
     streamFileUpload().catch(console.error);
     const solvername = fileUpload.name.slice(0, fileUpload.name.length-".tar.gz".length);
-    k8s().coreApi.createNamespacedPod("default", kanikoPod(solvername));
+    k8s().batchApi.createNamespacedJob("default", kanikoJob(solvername))
     return true; 
 }
