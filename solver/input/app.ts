e451b6647ce775e1ee0a88754702f453b8411bf2
majegit <68793210+majegit@users.noreply.github.com>
2022-01-23 21:25:34 +0100

    Update app.ts

--- solver/input/app.ts
+++ solver/input/app.ts
@@ -8,7 +8,7 @@ const fileBinaryConverter: FirestoreDataConverter<FileBinary> = {
     fromFirestore(snapshot: QueryDocumentSnapshot) {
         return snapshot.data() as FileBinary;
     }
-}
+} 
 
 interface FileBinary {
     id: string
